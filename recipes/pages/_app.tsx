33c023ac6f6e2c2ec924a9b748bc4ef32560a02a
Karen Jiang <kkarenjjiang.com>
2023-02-23 22:49:26 -0800

    flipcard

--- recipes/pages/_app.tsx
+++ recipes/pages/_app.tsx
@@ -1,6 +1,41 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import FlipCard from "../Components/RecipeCard";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.scss";
+
+const cards = [
+  {
+    id: "1",
+    variant: "hover",
+    front: "Hover",
+    back: "Back"
+  },
+  {
+    id: "2",
+    variant: "click",
+    front: "Click",
+    back: "Back"
+  },
+  {
+    id: "3",
+    variant: "focus",
+    front: "Focus",
+    back: "Back"
+  }
+];
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="row h-100">
+        <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
+          {cards.map((card) => (
+            <FlipCard key={card.id} card={card} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
